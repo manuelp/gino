307bb3e64480ce017bf4f2a3ca563a8fbe20d0f4
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-02-29 21:43:47 +0100

    Baseline

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject gino "0.1.0"
   :description "Compile and deploy Play! projects on remove *nix hosts via SSH."
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.apache.directory.studio/org.apache.commons.io "2.1"]])
--- src/gino/core.clj
+++ src/gino/core.clj
@@ -1,1 +1,20 @@
-(ns gino.core)
+(ns gino.core
+  (:import [org.apache.commons.io FileUtils]
+           [java.io File]))
+
+;; Requirements:
+;; - dirs have to exist
+;; - play should be in the PATH
+
+(def app-name "timesheet")
+(def workspace-dir "~/workspace")
+(def app-dir (str workspace-dir "/" app-name))
+(def dest (str "~/deploy/" app-name))
+(def tomcat-dir "/usr/share/tomcat6")
+
+(defn create-war-cmd [workspace-dir app-name version dest]
+  (str "play war " app-name " --%prod -o " dest "-" version))
+
+;; ---
+(defn remove-dir [directory]
+  (. FileUtils deleteDirectory (File. directory)))
