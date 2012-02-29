3fea424a63f0ae819f34ae84084c45e3d8f8dd4d
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-02-29 20:55:04 +0100

    Skeleton generated using Leiningen

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject gino "0.1.0"
+  :description "Compile and deploy Play! projects on remove *nix hosts via SSH."
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/gino/test/core.clj
@@ -0,0 +1,6 @@
+(ns gino.test.core
+  (:use [gino.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
