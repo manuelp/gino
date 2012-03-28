24c4999791f9e2a9ae0a66579538d0442b0632f0
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-03-28 23:35:34 +0200

    Can read the first line of remote cmd execution output

--- src/gino/core.clj
+++ src/gino/core.clj
@@ -1,10 +1,12 @@
 (ns gino.core
-  (:import [org.apache.commons.io FileUtils]
-           [java.io File]
+  (:import [org.apache.commons.io FileUtils IOUtils]
+           [java.io File BufferedReader InputStreamReader]
+           [java.util.concurrent TimeUnit]
            [org.apache.commons.exec CommandLine DefaultExecutor]
            [net.schmizz.sshj SSHClient]
            [net.schmizz.sshj.xfer FileSystemFile]
-           [net.schmizz.sshj.xfer.scp SCPFileTransfer]))
+           [net.schmizz.sshj.xfer.scp SCPFileTransfer]
+           [net.schmizz.sshj.transport.verification PromiscuousVerifier]))
 
 (defn build-cmd [cmd args]
   (loop [cmdline (new CommandLine cmd)
@@ -26,7 +28,6 @@
 (defn copy-to-file [file to-file]
   (. FileUtils copyFile (File. file) (File. to-file)))
 
-
 (defn remote-copy [file host username password dest]
   (let [ssh (new SSHClient)]
     (do (. ssh loadKnownHosts)
@@ -34,3 +35,16 @@
         (. ssh authPassword username password)
         (. ssh useCompression)
         (. (. ssh newSCPFileTransfer) upload (new FileSystemFile file) dest))))
+
+(defn read-cmd-output [is]
+  (.readLine (new BufferedReader (new InputStreamReader is))))
+
+;; PromiscuousVerifier is for: https://gist.github.com/1321719#gistcomment-62366
+(defn remote-cmd [host username password cmd]
+  (let [ssh (new SSHClient)]
+    (do (.addHostKeyVerifier ssh (new PromiscuousVerifier))
+        (.connect ssh host)
+        (.authPassword ssh username password)
+        (.useCompression ssh)
+        (.addHostKeyVerifier ssh "08:19:b9:c1:0b:33:71:bc:6e:24:db:45:3d:f4:a6:7b")
+        (read-cmd-output (.getInputStream (.exec (.startSession ssh) cmd))))))
