bc3a67482b9eec74421b2f2c268818863dc35b16
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-05-03 23:16:46 +0200

    Upgrade to lein2

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject gino "0.1.1-dev"
-  :description "Compile and deploy Play! projects on remove *nix hosts via SSH."
+(defproject gino/gino "0.1.1-dev" 
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.apache.directory.studio/org.apache.commons.io "2.1"]
+                 [org.apache.directory.studio/org.apache.commons.io
+                  "2.1"]
                  [org.apache.commons/commons-exec "1.1"]
                  [net.schmizz/sshj "0.7.0"]
                  [org.slf4j/slf4j-simple "1.6.4"]
-                 [com.jcraft/jzlib "1.1.1"]])
+                 [com.jcraft/jzlib "1.1.1"]]
+  :description "Compile and deploy Play! projects on remove *nix hosts via SSH.")
